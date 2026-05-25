379731e082687dbbaf4511a898557a9a58de3ce4
suraj <surajbele51@gmail.com>
2026-05-25 09:30:01 +0530

    first terraform file added

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "myec2" {
         ami = "ami-091138d0f0d41ff90"
                         instance_type = "t3.micro"
             key_name = "id_rsa"
-vpc_security_group_ids = [ "sg1"]
+vpc_security_group_ids = ["sg-0aed4cc5284ab673b"]
 tags = {
   Name = "myec2"  
   name = "suraj"
