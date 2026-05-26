228be61663e7609d9baf3b67685a3e7e528a5bd0
suraj <surajbele51@gmail.com>
2026-05-26 08:58:53 +0530

    terraform block added

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "batch23-123"
+    region = "us-east-1"
+  }
+}
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 resource "aws_instance" "myec2" {
-        ami = "ami-091138d0f0d41ff90"
-                        instance_type = "t3.micro"
-            key_name = "id_rsa"
-vpc_security_group_ids = ["sg-0aed4cc5284ab673b"]
-tags = {
-  Name = "myec2"  
-  name = "suraj"
-  env = "dev"
-}
+  ami                    = "ami-091138d0f0d41ff90"
+  instance_type          = "t3.micro"
+  key_name               = "id_rsa"
+  vpc_security_group_ids = ["sg-0aed4cc5284ab673b"]
+  tags = {
+    Name = "myec2"
+    name = "suraj"
+    env  = "dev"
+  }
 }
