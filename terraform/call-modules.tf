b2d06a4c78f3e7d45637916226dfc426cdef8007
suraj <surajbele51@gmail.com>
2026-06-02 09:41:59 +0530

    ansible added

--- terraform/call-modules.tf
+++ terraform/call-modules.tf
@@ -8,7 +8,16 @@ module "myvpc" {
   az2 = var.az2
   route-cidr = var.route-cidr
 }
-
+module "myvpc" {
+  source = "modules/vpc"
+  region = "us-east-1"
+  vpc-cidr = var.vpc-cidr2
+  pub-sub-cidr = var.pub-sub-cidr2
+  pvt-sub-cidr = var.pvt-sub-cidr2
+  az1 = var.az1
+  az2 = var.az2
+  route-cidr = var.route-cidr
+}
 module "myinstance" {
   source = "modules/instance"
   ami = var.ami
@@ -16,6 +25,7 @@ module "myinstance" {
   key_name = var.key_name
   mysg = aws_security_group.sg1.id
   instance-name1 = var.instance-name
+  subnet = module.myvpc.vpc_id
 }
 resource "aws_security_group" "sg1" {
   name        = "${var.project}-sg"
